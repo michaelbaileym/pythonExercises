7f03039c243821d54997f2d0acd089bc30774fe2
Michael Bailey <michaelbailey@Michaels-MacBook-Pro.local>
2018-09-17 21:07:39 -0700

    dictionary problem

--- source/454e.py
+++ source/454e.py
@@ -30,10 +30,7 @@ def name_lists(name_list):
     for fullname in nameSet:
         nameSet[fullname].sort()
     return nameSet
-    
-    
-    
-            
+
 #Below are some lines of code that will test your function.
 #You can change the value of the variable(s) to test your
 #function with different inputs.
